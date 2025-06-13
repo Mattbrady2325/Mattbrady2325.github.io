380bf597854c0fa5ef7dd0177a720f0fd55d02a5
Matt Brady <matthew.brady@geeksi.tech>
2025-06-13 17:03:51 -0400

    Edited about me page and applied gradient added footer and logos added js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
+//function for showing sidebar
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
 }
-
+//function for hiding sidebar
 function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
+}
+
+
+function bringToFront(boxId) {
+    const box1 = document.getElementById("box1");
+    const box2 = document.getElementById("box2");
+
+    box1.classList.remove("active");
+    box2.classList.remove("active");
+
+    document.getElementById(boxId).classList.add("active");
 }
